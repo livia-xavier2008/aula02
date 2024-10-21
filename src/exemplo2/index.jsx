2d82c81cfe8e1cee24b517f97e4b268872e5379c
livia-xavier2008 <159331621+livia-xavier2008@users.noreply.github.com>
2024-10-21 14:22:44 -0400

    atividade
    ok

--- src/exemplo2/index.jsx
+++ src/exemplo2/index.jsx
@@ -2,24 +2,26 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   
-  const [??,??] = useState([]);
+  const [lista,setlista] = useState([]);
 
   useEffect(() => { 
-
     const buscarUsuario  = async () => {
-                    = await fetch('https://jsonplaceholder.typicode.com/photos');
-        const dados =                ;
+    const resposta = await fetch('https://jsonplaceholder.typicode.com/photos');
+    const dados = await resposta.json();
         setFotos(dados);
     }
     buscarUsuario();
-  }, //complete o código);
+  },[]);
 
   return (
     <>
       <h1>Galeria de Fotos</h1>
       <ul>
-        {}
+        {lista.map(produto =>
+          <li key={produto.id}>
+          </li>
+          )}
       </ul>
     </>
   );
-}
+ }
--- src/exemplo3/index.jsx
+++ src/exemplo3/index.jsx
@@ -2,25 +2,27 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   
-  const [??,??] = useState([]);
+  const [lista,setlista] = useState([]);
 
   useEffect(() => { 
 
-    const  ????  = async () => {
+    const buscarUsuario  = async () => {
         const resposta = await fetch('https://randomuser.me/api');
         const dados = await resposta.json();
-         //complete o código
+         setlista (dados)  
     }
-    //complete o código
+    buscarUsuario()
   }, []);
 
   return (
     <>
       <h1>Usuário</h1>
       <ul>
-        {
-            //complete o código
-        }
+        {lista.map(produto =>
+        <li key={produto.id}>
+
+        </li>
+          )}
       </ul>
     </>
   );
